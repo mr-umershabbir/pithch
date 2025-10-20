fe8195a71b4af927bbd313545dcbcea5d0022672
umer3221 <umershabbir388@gmail.com>
2025-10-20 20:42:02 +0500

    .

--- api/generate.js
+++ api/generate.js
@@ -25,7 +25,7 @@ module.exports = {
       `;
 
       const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_KEY}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_KEY}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
